837f5921455b213a237d1a528b728e4bcc5ab382
Alin Luca <alinluca@smallfootprint.com>
2016-01-18 16:39:08 +0200

    Controllers & Markup chapter - end

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -3,9 +3,19 @@
 eventsApp.controller('EventController',
     function EventController($scope) {
 
+        $scope.snippet = '<span style="color:red;">Span</span>';
+
+        $scope.boolValue = true;
+
+        $scope.mystyle = {color:'red'};
+
+        $scope.buttonDisabled = true;
+
+        $scope.sortorder = 'name';
+
         $scope.event = {
             name: 'Angular Boot Camp',
-            date: '1/1/2016',
+            date: 1359781015626,
             time: '10:30 am',
             location: {
                 address: 'Google Headquarters',
@@ -15,28 +25,39 @@ eventsApp.controller('EventController',
             imageUrl: '/img/angularjs-logo.png',
             sessions: [
                 {
-                    name: 'Directives Masterclass',
+                    name: 'Directives Masterclass monica',
                     creatorName: 'Bob Smith',
-                    duration: '1h',
+                    duration: 1,
                     level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!'
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'Bob Smith',
-                    duration: '1h',
+                    duration: 2,
                     level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!'
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
                 },
                 {
                     name: 'Well behaved controllers',
                     creatorName: 'Bob Smith',
-                    duration: '1h',
+                    duration: 4,
                     level: 'Advanced',
-                    abstract: 'In this session you will learn the ins and outs of directives!'
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
                 }
             ]
         }
+
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
+        };
 
     }
 );
